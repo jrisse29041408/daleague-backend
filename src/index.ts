f90f26b193b079df758f2ef0377d9a0f3148dfdf
Jack Risse <j.risse14@gmail.com>
2020-06-16 17:09:46 -0500

    server setup

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return 'hi';
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
+
+  });
+
+  const apolloServer = new ApolloServer({
+    schema
+  })
+
+  const app = express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server listening on http://localhost:4000/graphql");
+  });
+};
+
+main();
